043362e4b9aa3933e57b8cdd28775d5309e07ccc
ArpitDwivedi7 <dwivediarpit@hotmail.com>
2023-01-28 01:36:54 +0530

    TestClass2

---
+++ sample1/src/myPackage/TestCalss2.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class TestCalss2 
+{
+	public static void main(String[]args)
+	{
+		System.out.println("BHARAT");	
+		System.out.println("MATA");
+		System.out.println("KI");
+		System.out.println("JAI");
+	}
+}
