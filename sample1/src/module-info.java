d3b11a2aba6d405d015484693ab1691c0410d886
ArpitDwivedi7 <dwivediarpit@hotmail.com>
2023-01-28 00:46:23 +0530

    PrintingStatement

---
+++ sample1/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author dwive
+ *
+ */
+module sample1 {
+}
---
+++ sample1/src/myPackage/TestClass.java
@@ -0,0 +1,13 @@
+package myPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("RAM RAM");
+		System.out.println(1234);
+		System.out.println("RADHEY SHYAM");
+		System.out.println("5678");
+	}
+
+}
